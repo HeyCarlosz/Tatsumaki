ab1f787cff169c0690a09df1cc9db2af9aa9994d
Carlos <49657335+HeyCarlosz@users.noreply.github.com>
2020-02-14 16:04:04 -0300

    Add files via upload

---
+++ comandos/ping.js
@@ -0,0 +1,5 @@
+exports.run = (client, message, args)  => {
+    message.channel.send(`:ping_pong: | Calculando latência, aguarde.`).then(msg1 => {
+        msg1.edit(`${message.author}\n🏓 | Latência da mensagem editada: ` + `**${Date.now() - msg1.createdTimestamp}**` +'ms\n❤ | Batimento cardiaco: (ping - websocket) ' +Math.round(client.ping)+`ms`)
+    })
+}
