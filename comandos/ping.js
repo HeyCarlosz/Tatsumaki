c832b488f93edacd9959aa931b4cd6f32933aa0e
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-02-14 17:27:22 -0300

    funcionando

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,5 +1,9 @@
-exports.run = (client, message, args)  => {
-    message.channel.send(`:ping_pong: | Calculando latência, aguarde.`).then(msg1 => {
-        msg1.edit(`${message.author}\n🏓 | Latência da mensagem editada: ` + `**${Date.now() - msg1.createdTimestamp}**` +'ms\n❤ | Batimento cardiaco: (ping - websocket) ' +Math.round(client.ping)+`ms`)
-    })
-}
+module.exports = {
+	name: 'ping',
+	description: 'Ping!',
+	execute(message, args) {
+		message.channel.send(`:ping_pong: | Calculando latência, aguarde.`).then(msg1 => {
+			msg1.edit(`${message.author}\n🏓 | Latência da mensagem editada: ` + `**${Date.now() - msg1.createdTimestamp}**` + 'ms\n❤ | Batimento cardiaco: (ping - websocket) ' + Math.round(message.client.ping) + `ms`)
+		});
+	},
+};
--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 console.log("Starting...")
+const fs = require('fs');
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client({
-    autoReconnect: true,
-    disableEveryone: true
+	autoReconnect: true,
+	disableEveryone: true
 });
-const botconfig = require("./config.json");
+client.commands = new Discord.Collection();
 
-client.on('ready', () => {
-    console.log(`Hello World! Logged in as ${client.user.tag}!\n\n`);
-    client.user.setPresence({
-        status: "idle",
-        game: {
-            name: `${client.users.size} seres humanos!`,
-            type: "WATCHING"
-        }
-    })
-})
-  
-client.on('message', async message => {
-    if(message.author.bot) return;
-    if(message.channel.type == "dm") return;
+const commandFiles = fs.readdirSync('./comandos').filter(file => file.endsWith('.js'));
 
-    const prefix = botconfig.prefix
-    let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);        
-          let args = message.content.slice(prefix.length).trim().split(' ');
-          if (!message.content.startsWith(prefix)) return;
-            try {
-                delete require.cache[require.resolve(`./comandos/${command}.js`)];
-                let commandFile = require(`./comandos/${command}.js`)
-                commandFile.run(client, message, args, prefix)
-            } catch (erro) {
-                console.log(erro.stack);
-}});
+for (const file of commandFiles) {
+	const command = require(`./comandos/${file}`);
+	client.commands.set(command.name, command);
+}
 
-client.on('message', async message => {
-    if(message.author.bot) return;
-    if(message.channel.type == "dm") return
-    const prefixo = botconfig.prefix
-
-    if(message.content.includes(`<@${client.user.id}>`) || message.content.includes(`<@!${client.user.id}>`)) {
-        message.delete();
-          
-        message.channel.send(`Onii-chan ${message.author}, use ${prefixo}ajuda para ver meu painel de ajuda! <a:disclove:677169521387110412>`)
-    }
+client.once('ready', () => {
+	console.log(`Hello World! Logged in as ${client.user.tag}!\n\n`);
+	client.user.setPresence({
+		status: "idle",
+		game: {
+			name: `${client.users.size} seres humanos!`,
+			type: "WATCHING"
+		}
+	})
 });
 
-client.login(botconfig.token)
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(erro.stack);
+		message.reply('Tivemos um erro no comando que você pediu!');
+	}
+});
+
+client.login(token);
