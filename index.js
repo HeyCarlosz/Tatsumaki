9d8bf4b49b92dce6cdff200d81555c5b7174e719
Carlos <49657335+HeyCarlosz@users.noreply.github.com>
2020-02-16 19:46:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,46 +4,32 @@ const client = new Discord.Client({
     autoReconnect: true,
     disableEveryone: true
 });
+const fs = require("fs"); 
+const Enmap = require("enmap");
 const botconfig = require("./config.json");
 
-client.on('ready', () => {
-    console.log(`Hello World! Logged in as ${client.user.tag}!\n\n`);
-    client.user.setPresence({
-        status: "idle",
-        game: {
-            name: `${client.users.size} seres humanos!`,
-            type: "WATCHING"
-        }
-    })
-})
-  
-client.on('message', async message => {
-    if(message.author.bot) return;
-    if(message.channel.type == "dm") return;
-
-    const prefix = botconfig.prefix
-    let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);        
-          let args = message.content.slice(prefix.length).trim().split(' ');
-          if (!message.content.startsWith(prefix)) return;
-            try {
-                delete require.cache[require.resolve(`./comandos/${command}.js`)];
-                let commandFile = require(`./comandos/${command}.js`)
-                commandFile.run(client, message, args, prefix)
-            } catch (erro) {
-                console.log(erro.stack);
-}});
-
-client.on('message', async message => {
-    if(message.author.bot) return;
-    if(message.channel.type == "dm") return
-    const prefixo = botconfig.prefix
-
-    if(message.content.includes(`<@${client.user.id}>`) || message.content.includes(`<@!${client.user.id}>`)) {
-        message.delete();
-          
-        message.channel.send(`Onii-chan ${message.author}, use ${prefixo}ajuda para ver meu painel de ajuda! <a:disclove:677169521387110412>`)
-    }
+client.commands = new Enmap();
+fs.readdir("./comandos/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+      if (!file.endsWith(".js")) return;
+      let props = require(`./comandos/${file}`);
+      let commandName = file.split(".")[0];
+      client.commands.set(commandName, props);
+  });
 });
 
-client.login(botconfig.token)
+fs.readdir("./eventos/", (err, files) => { 
+    if (err) return console.error(err);
+    files.forEach(file => { 
+        let eventFunction = require(`./eventos/${file}`); 
+        let eventName = file.split(".")[0]; 
+        client.on(eventName, (...args) => 
+        eventFunction.run(client, ...args
+        )); 
+    }); 
+});
+
+client.login(botconfig.token).catch(aa => {
+    console.log(`Ocorreu um erro ao tentar logar.\n${aa}`);
+})
