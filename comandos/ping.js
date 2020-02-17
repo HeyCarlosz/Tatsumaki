92ce07e6bb4dd7ef3d6fd031263efc114df302f5
Carlos <49657335+HeyCarlosz@users.noreply.github.com>
2020-02-16 21:03:20 -0300

    Update ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,4 +1,6 @@
-exports.run = (client, message, args)  => {
+const Discord = require('discord.js')
+
+exports.run = (/** @type {Discord.Client} */ client, /** @type {Discord.Message} */ message, args)  => {
     message.channel.send(`:ping_pong: | Calculando latência, aguarde.`).then(msg1 => {
         msg1.edit(`${message.author}\n🏓 | Latência da mensagem editada: ` + `**${Date.now() - msg1.createdTimestamp}**` +'ms\n❤ | Batimento cardiaco: (ping - websocket) ' +Math.round(client.ping)+`ms`)
     })
